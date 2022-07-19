c6e6722fd479036dffe6c274f9fee3339b16311a
sadegh-dev <sadegh.rez@gmail.com>
2022-07-19 14:55:42 +0430

    try-except in 0-connect.py

--- 0-connect.py
+++ 0-connect.py
@@ -4,12 +4,16 @@
 
 import psycopg2
 
-conn = psycopg2.connect(
-    dbname = "mydb" ,
-    user = "charlie" , 
-    password = "charlie" ,
-    host = "localhost" ,
-    port = "5432" ,
-)
+try :
+    conn = psycopg2.connect(
+        dbname = "mydb" ,
+        user = "charlie" , 
+        password = "charlie" ,
+        host = "localhost" ,
+        port = "5432" ,
+    )
+    print('Connection Successful ..!')
+except :
+    print('Connection Failed ..!')
 
 conn.close()
