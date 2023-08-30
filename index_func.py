419e2b9ea9affe12343a15bdfc33eaebb282987d
sadegh-dev <sadegh.rez@gmail.com>
2023-08-30 17:41:47 +0330

    Determine the page size

--- index_func.py
+++ index_func.py
@@ -3,8 +3,19 @@ from tkinter import *
 
 
 # Create Tk instance
-
 window = Tk()
+
+
+# set size tab
+"""
+window.minsize(400,300)
+window.maxsize(600,500)
+
+"""
+window.geometry("600x500")
+window.resizable(width=False, height=False)
+
+
 
 
 # Create run loop
