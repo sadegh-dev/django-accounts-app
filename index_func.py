460c8bd2b17aebdf8db8a343ac28b82d86eeedef
sadegh-dev <sadegh.rez@gmail.com>
2023-09-01 18:21:28 +0330

    insert use_counter

--- index_func.py
+++ index_func.py
@@ -1,5 +1,10 @@
-
 from tkinter import *
+
+counter = 0
+def action_butt():
+    global counter
+    counter += 1
+    count.config(text="Number of uses : {}".format(counter))
 
 
 # Create Tk instance
@@ -10,12 +15,46 @@ window = Tk()
 """
 window.minsize(400,300)
 window.maxsize(600,500)
-
 """
+
+
 window.geometry("600x500")
 window.resizable(width=False, height=False)
 
 
+# The name of the program page
+window.title('Calculate BMI')
+
+
+# Title of the program
+Label(window, text="Calculate body BMI",
+      font=("arial", 14),
+      fg="blue", bg="white").pack()
+
+
+# counter
+count = Label(window, font=("arial", 14), fg="white", bg="blue")
+count.config(text="Number of uses : 0")
+count.pack()
+
+
+# Definition of Gender
+gender = Label(window, text="Male / Female")
+gender.config(fg="red")
+gender.pack()
+
+
+# Calculate button
+cal_butt = Button(window, text="Enter")
+cal_butt.config(bg="yellow", fg="black")
+cal_butt.config(command=action_butt)
+cal_butt.pack()
+
+
+# Developer name
+Label(window, text="Developed by Sadegh", 
+      font=("Tahoma", 12), 
+      foreground="white", background="blue" ).pack()
 
 
 # Create run loop
