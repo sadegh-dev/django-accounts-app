e87d069be8b8617f288e1785e584acd96ceffc8a
sadegh-dev <sadegh.rez@gmail.com>
2023-10-12 16:48:59 +0330

    create User and Profile models-class

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+
+class User(AbstractBaseUser):
+    pass
+
+
+class Profile():
+    pass
+
+
