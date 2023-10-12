1dbe0c8542c64d1abaa7f6fc977e556d153c4ada
sadegh-dev <sadegh.rez@gmail.com>
2023-10-12 17:15:43 +0330

    definition of create_ser, create_superuser functions

--- accounts/managers.py
+++ accounts/managers.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class MyUserManager(BaseUserManager):
-    pass
+    def create_user():
+        pass
+
+    def create_superuser():
+        pass
 
