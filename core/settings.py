29026499b576bd72f82bdb52e8c1d1ba5236d864
sadegh-dev <sadegh.rez@gmail.com>
2023-10-15 18:15:23 +0330

    set the AUTH_USER_MODEL in the settings file

--- core/settings.py
+++ core/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Authentication
+
+AUTH_USER_MODEL = 'accounts.User'
+
