920a745976c3f1f85d743f811b4e5d8bec27ec00
sadegh-dev <sadegh.rez@gmail.com>
2023-10-24 23:32:44 +0330

    add is_admin and is_active fields in User model class

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,9 @@ from django.contrib.auth.models import AbstractBaseUser
 class User(AbstractBaseUser):
     email = models.EmailField(max_length=254, unique=True)
 
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
 
 
 class Profile(models.Model):
