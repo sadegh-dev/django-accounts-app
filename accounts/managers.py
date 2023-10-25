2f4ff95cadc787052063828911744a72652be655
sadegh-dev <sadegh.rez@gmail.com>
2023-10-25 19:07:49 +0330

    get email in create_user method in CustomUserManager and process that

--- accounts/managers.py
+++ accounts/managers.py
@@ -4,7 +4,9 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
-        pass
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
 
     def create_superuser(self, email, password, **extra_fields):
         pass
