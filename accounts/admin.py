a8bc7b353c5f390fe294e04d24eded023367ab91
sadegh-dev <sadegh.rez@gmail.com>
2023-11-02 23:57:50 +0330

    admin.site.register for User class and UserAdmin class

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
 
 
 class UserAdmin(BaseUserAdmin):
     pass
 
+
+
+admin.site.register(User, UserAdmin)
