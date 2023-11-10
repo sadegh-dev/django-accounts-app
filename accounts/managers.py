b4e9a39f0578afa58842fae0d05fac2d0880037f
sadegh-dev <sadegh.rez@gmail.com>
2023-11-10 19:04:59 +0330

    write return statement in create_superuser method in managers file

--- accounts/managers.py
+++ accounts/managers.py
@@ -4,8 +4,10 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
+
         if not email:
             raise ValueError(_("Email is required"))
+        
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -23,4 +25,6 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_('superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('superuser must have is_superuser=True.'))
+        
+        return self.create_user(email, password, **extra_fields)
         
