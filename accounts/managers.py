ca645dbac35ba5102c7ece2584bd82cba31b0fef
sadegh-dev <sadegh.rez@gmail.com>
2023-11-10 18:49:58 +0330

    Write the condition that the email is necessary in the managers file

--- accounts/managers.py
+++ accounts/managers.py
@@ -4,6 +4,8 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("Email is required"))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
